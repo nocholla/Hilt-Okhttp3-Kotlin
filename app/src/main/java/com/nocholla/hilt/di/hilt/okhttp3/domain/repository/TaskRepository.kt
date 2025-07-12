4cddc48f1635e812f723694972102c9081f7ed4e
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-11 20:03:14 -0700

    Update TaskRepository.kt

--- app/src/main/java/com/nocholla/hilt/di/hilt/okhttp3/domain/repository/TaskRepository.kt
+++ app/src/main/java/com/nocholla/hilt/di/hilt/okhttp3/domain/repository/TaskRepository.kt
@@ -1,4 +1,11 @@
 package com.nocholla.hilt.di.hilt.okhttp3.domain.repository
 
-class TaskRepository {
+import com.nocholla.hilt.di.hilt.okhttp3.domain.model.Task
+import kotlinx.coroutines.flow.Flow
+
+interface TaskRepository {
+    suspend fun getTasks(): Flow<List<Task>>
+    suspend fun addTask(name: String): Task
+    suspend fun toggleTaskCompletion(task: Task): Task
+    suspend fun removeTask(taskId: Int)
 }
