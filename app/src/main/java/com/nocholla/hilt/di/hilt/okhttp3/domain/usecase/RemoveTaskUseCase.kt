6f1e80567a83487ed4b95d68d1433a878e7eeeeb
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-11 20:06:51 -0700

    Update RemoveTaskUseCase.kt

--- app/src/main/java/com/nocholla/hilt/di/hilt/okhttp3/domain/usecase/RemoveTaskUseCase.kt
+++ app/src/main/java/com/nocholla/hilt/di/hilt/okhttp3/domain/usecase/RemoveTaskUseCase.kt
@@ -1,4 +1,15 @@
 package com.nocholla.hilt.di.hilt.okhttp3.domain.usecase
 
-class RemoveTaskUseCase {
+import com.nocholla.hilt.di.hilt.okhttp3.domain.repository.TaskRepository
+import javax.inject.Inject
+
+class RemoveTaskUseCase @Inject constructor(private val repository: TaskRepository) {
+    suspend operator fun invoke(taskId: Int): Result<Unit> {
+        return try {
+            repository.removeTask(taskId)
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
