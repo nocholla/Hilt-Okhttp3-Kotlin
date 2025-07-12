18b42809806f1500b7c03f4e2aa0072d8b3627de
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-11 19:49:49 -0700

    Update MainActivity.kt

--- app/src/main/java/com/nocholla/hilt/di/hilt/okhttp3/presentation/view/MainActivity.kt
+++ app/src/main/java/com/nocholla/hilt/di/hilt/okhttp3/presentation/view/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nocholla.hilt.di.hilt.okhttp3.presentation.theme.HiltOkhttp3Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
