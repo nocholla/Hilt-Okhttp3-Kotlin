f0d7d8a59ccbb18ea34f06542610d6d12057a821
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-11 20:05:39 -0700

    Update AddTaskUseCase.kt

--- app/src/main/java/com/nocholla/hilt/di/hilt/okhttp3/domain/usecase/AddTaskUseCase.kt
+++ app/src/main/java/com/nocholla/hilt/di/hilt/okhttp3/domain/usecase/AddTaskUseCase.kt
@@ -1,4 +1,16 @@
 package com.nocholla.hilt.di.hilt.okhttp3.domain.usecase
 
-class AddTaskUseCase {
+import com.nocholla.hilt.di.hilt.okhttp3.domain.model.Task
+import com.nocholla.hilt.di.hilt.okhttp3.domain.repository.TaskRepository
+import javax.inject.Inject
+
+class AddTaskUseCase @Inject constructor(private val repository: TaskRepository) {
+    suspend operator fun invoke(name: String): Result<Task> {
+        return try {
+            val task = repository.addTask(name)
+            Result.success(task)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
